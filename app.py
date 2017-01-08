6bfcc8c1a2b913a112ec5681ba040478e2f6676f
Jonathan Buhler <benedict.buhler@gmail.com>
2017-01-08 15:59:00 -0500

    Logging in and database stuff

--- app.py
+++ app.py
@@ -1,6 +1,52 @@
 from flask import Flask, render_template, url_for, request, redirect, session
-
+from utils import auth
 app = Flask(__name__)
 app.secret_key = 'pineapples'
 
 @app.route('/')
+def root():
+    if not isUser():
+        return redirect(url_for('login'))
+    return redirect(url_for('home'))
+
+@app.route('/home/')
+def home():
+    if not isUser():
+        return redirect(url_for('login'))
+    return render_template('home.html', user = session['user'])
+
+@app.route('/login/')
+def login():
+    if isUser():
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/authenticate/', methods = ['POST'])
+def authenticate():
+    data = [request.form['username'], request.form['password'], request.form['gender']]
+    data = auth.login(data)
+    if data[1]:
+        session['user'] = data[2]
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html', message = data[0])
+
+@app.route('/create1/', methods = ['POST'])
+def create1():
+    if isUser():
+        return redirect(url_for('home'))
+
+@app.route('/logout/')
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('login'))
+
+def isUser():
+    if not ('user' in session):
+        return False
+    return True
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ utils/auth.py
@@ -0,0 +1,41 @@
+import hashlib, sqlite3
+
+def hashPass(password):
+    return hashlib.sha512(password).hexdigest()
+
+def register(user, password):
+    result = []
+    bd = sqlite3.connect('data/bd.db')
+    c = bd.cursor()
+    if (userExists(user, c)):
+        result = ['User already exists.', False]
+    elif not user.isalnum() or not password.isalnum():
+        result = ['Username and password may only consist of alphanumeric characters.', False]
+    else:
+        p = hashPass(password)
+        c.execute("INSERT INTO USERS VALUES ('%s', '%s')"%(user, p))
+        bd.commit()
+        bd.close()
+        result = ['Registration successful.', False]
+    return result
+
+def login(data):
+    bd = sqlite3.connect('data/bd.db')
+    c = bd.cursor()
+    if not userExists(data[0], c):
+        return ['Username does not exist.', False]
+    else:
+        s = c.execute("SELECT PASSWORD FROM USERS WHERE USERNAME = '%s'" %(data[0]))
+        p = s.fetchone()[0]
+        if (p != hashPass(data[1])):
+            result = ['Incorrect password.', False]
+        else:
+            result = ['Login successful.', True, data[0]]
+    return result
+
+def userExists(username, c):
+    s = c.execute("SELECT USERNAME FROM USERS")
+    for r in s:
+        if username == r[0]:
+            return True
+    return False
