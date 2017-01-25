4ff5519340222e31d4ec0f4c5d83fde2c9160a9f
esandine <esandine@stuy.edu>
2017-01-25 12:59:24 -0500

    Match button routing works

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect, session
 from utils import auth, tmdb, message
 import json
-
 app = Flask(__name__)
 app.secret_key = 'pineapples'
 
@@ -68,6 +67,11 @@ def process():
     ret = {'results':ret}
     print ret
     return json.dumps(ret)
+
+@app.route('/match/', methods = ['POST'])
+def match():
+    user=request.form['user']
+    print user
 
 if __name__ == '__main__':
     app.debug = True
